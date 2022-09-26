383834ba5d00d37d442e6c14099d2dcf561bbd6f
Mouse-Diplodicus <williamm1.matt@yahoo.com>
2022-09-25 19:29:34 -0600

    Got simulation and replays working. Also implemented very basic scoring function.

--- main.py
+++ main.py
@@ -3,15 +3,17 @@ import cityflow
 
 class Model:
 
-    def __init__(self, config_path, num_threads = 1):
+    def __init__(self, config_path, num_threads=1):
         self.config_path = config_path
         self.engine = cityflow.Engine(config_path, thread_num=num_threads)
-        self.age = age
 
     def simulate(self, num_steps):
+        wait_time = 0
         for i in range(0, num_steps):
-            print(self.engine.get_vehicle_count())
+            # since interval is 1 wait_time is sum of the number of vehicles waiting
             self.engine.next_step()
+            wait_time += sum(self.engine.get_lane_waiting_vehicle_count().values())
+        print(wait_time)
 
 
 if __name__ == '__main__':

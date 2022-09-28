48c85a1bb506c00d357e671406e1af28038c28ee
Mouse-Diplodicus <williamm1.matt@yahoo.com>
2022-09-28 15:50:07 -0600

    Started work on getting a gym model working

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
-import cityflow
+import timeit
 
+import cityflow
+import json
+import gym
+import gym_cityflow
+import numpy as np
 
 class Model:
 
-    def __init__(self, config_path, num_threads=1):
-        self.config_path = config_path
-        self.engine = cityflow.Engine(config_path, thread_num=num_threads)
+    def __init__(self, config_path, num_steps):
+        self.num_steps = num_steps
+        self.env = gym.make('cityflow-v0', configPath=config_path, episodeSteps=num_steps)
+        self.env.action_space.seed(42)
 
-    def simulate(self, num_steps):
-        wait_time = 0
-        for i in range(0, num_steps):
-            # since interval is 1 wait_time is sum of the number of vehicles waiting
-            self.engine.next_step()
-            wait_time += sum(self.engine.get_lane_waiting_vehicle_count().values())
-        print(wait_time)
+    def simulate(self):
+        self.env.reset()
+        start_time = timeit.default_timer()
+        while True :
+            observation, reward, done, debug = self.env.step(action=self.env.action_space.sample())
+            if done:
+                elapsed_time = round(timeit.default_timer() - start_time, 3)
+                print(f"Elapsed Time: {elapsed_time} seconds")
+                break
+
 
 
 if __name__ == '__main__':
-    test = Model("examples/config.json")
-    test.simulate(1000)
+    test = Model("examples/config.json", 1000)
+    test.simulate()
